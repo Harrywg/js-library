ca859fe7315a1d13adf7f29d5d3044d4a0d41cc5
Harry Ward-Gray <wardgray24@gmail.com>
2023-08-30 19:59:34 +0100

    built out simple test app

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 import { createEl } from "./library.js";
 
-const header = createEl("header");
-
-const title = createEl("h1", header);
-title.setEl((h1) => (h1.innerText = "This is my title"));
-
-const nav = createEl("nav", header);
-
-const main = createEl("main");
-const mainIntro = createEl("section", main);
-const mainIntroP1 = createEl("p", mainIntro);
-mainIntroP1.setEl((p) => {
-  p.innerText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium
-suscipit delectus explicabo vitae veritatis dignissimos. Repudiandae
-dolorum enim eligendi modi fugiat vero laudantium! Sapiente, minima. Quod
-vitae reprehenderit alias blanditiis? `;
-});
-
-const mouseElement = createEl("div");
-
-mouseElement.setEl((div) => {
-  div.id = "mouse-element";
-  div.style.background = `red`;
-  div.style.height = `100px`;
-});
-
-document.addEventListener("mousemove", (e) => {
-  mouseElement.setEl((div) => {
-    div.style.height = `${e.clientX / 2}px`;
+const createHeader = () => {
+  const header = createEl("header");
+  const title = createEl("h1", header);
+  title.setEl((h1) => {
+    h1.innerText = "Pokemon Search";
   });
-});
+};
 
-const footer = createEl("footer");
+createHeader();
+
+const createMain = () => {
+  const main = createEl("main");
+  const cardList = createCardList(main);
+};
+
+const createCardList = (main) => {
+  const cardList = createEl("div", main);
+  cardList.setEl((div) => (div.id = "card-list"));
+  fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((res) => {
+      return res.json();
+    })
+    .then(({ results }) => {
+      results.map((pokemon) => createCard(cardList, pokemon));
+    });
+};
+
+const createCard = (cardList, pokemon) => {
+  const card = createEl("div", cardList);
+  card.setEl((div) => (div.className = "card"));
+  const cardImg = createEl("img", card);
+  const cardP = createEl("p", card);
+  fetch(pokemon.url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((pokeData) => {
+      cardP.setEl((p) => (p.innerText = pokemon.name));
+      cardImg.setEl((img) => (img.src = pokeData.sprites.front_default));
+    });
+};
+
+createMain();
